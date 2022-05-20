3f1f3d19b54585ef3c20bc86dafeb2ac6849f30a
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-05-20 09:44:39 +0500

    create folders

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store";
-import App from "./App";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+root.render(<App />);
